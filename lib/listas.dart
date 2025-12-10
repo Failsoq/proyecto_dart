e097226c33a46164da434fbfae12ab6ed37f7a98
Failsoq <failsoq@gmail.com>
2025-12-10 10:47:02 -0400

    commit

---
+++ lib/listas.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: avoid_function_literals_in_foreach_calls
+
+import 'utilidades.dart' as util;
+
+void main(){
+  // Definir variables
+
+  util.encabezado('lista de edades', 50);
+
+  List<int> edades = [17,21,19,21,17,15,23,17,19,25,28,14];
+
+  util.linea(50);
+  print('Cantidad de edades es ${edades.length}'); //.lenght es un metodo y se usa para contar la cantidad
+                                                   // de valores que tiene la lista
+  util.linea(50);
+  print('La tercera edad es ${edades[2]}');
+  util.linea(50);
+  print('La quinta edad es ${edades.elementAt(4)}');
+  util.linea(50);
+  print('La ultima edad es ${edades.last}');
+  //print('La ultima edad es ${edades[edades.length - 1]}');
+  util.linea(50);
+  edades.add(29);
+  print(edades);
+  util.linea(50);
+  edades.addAll([24, 33, 27]);
+  print(edades);
+  util.linea(50);
+  print('Porción de la lista edades ${edades.sublist(2,6)}'); //Esto es para tomar una porcion de la lista
+  util.linea(50);
+  
+  util.encabezado('leer lista de edades (for i)', 50);
+   for (var i = 0; i < edades.length; i++) {
+     print('La edad $i es ${edades[i]}');
+   }
+  util.encabezado('leer lista de edades', 50);
+  for (var edad in edades) {
+    print('La edad es $edad');
+  }
+  /*for (var i = 0; i < edades.length; i++) {
+    
+  }*/
+  util.encabezado('leer lista de edades (for each)', 50);
+  edades.forEach((edad)=>print('la edad es $edad'));
+  
+  util.encabezado('edades ordenadas ascendente', 50);
+  edades.sort();  
+  edades.forEach((edad) => print('La edad es $edad'));
+
+  util.encabezado('edades ordenadas descendente', 50);
+
+
+
+}
